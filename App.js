d1ff983556a8a6ce53cad6a15548c48780b19dd6
Divine Orji <dpkreativ@gmail.com>
2022-06-05 15:20:41 +0100

    Enable location permissions

--- App.js
+++ App.js
@@ -1,7 +1,29 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        setErrorMessage('Access to location is needed to run the app');
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    } catch (error) {}
+  }
   return (
     <View style={styles.container}>
       <Text>Weather App</Text>
