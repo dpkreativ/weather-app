01a5a65f15d4b3265b82a7e8856afd34627b11bd
Divine Orji <dpkreativ@gmail.com>
2022-06-07 04:21:18 +0100

    Set temperature unit system

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather';
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [tempUnit, setTempUnit] = useState('metric');
 
   useEffect(() => {
     load();
@@ -27,7 +28,7 @@ export default function App() {
 
       const { latitude, longitude } = location.coords;
 
-      const weatherUrl = `${BASE_WEATHER_URL}?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
+      const weatherUrl = `${BASE_WEATHER_URL}?lat=${latitude}&lon=${longitude}&units=${tempUnit}&appid=${WEATHER_API_KEY}`;
 
       const response = await fetch(weatherUrl);
       const result = await response.json();
