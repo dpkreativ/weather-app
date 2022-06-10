13f7f3e99a24ffe658ac333d2f08870a714fc76c
Divine Orji <dpkreativ@gmail.com>
2022-06-10 10:08:34 +0100

    Set up weather icon

--- components/WeatherInfo.js
+++ components/WeatherInfo.js
@@ -1,13 +1,19 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import React from 'react';
 
 export default function WeatherInfo({ weather }) {
   const {
     main: { temp },
+    weather: [details],
   } = weather;
+
+  const { icon } = details;
+
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`;
 
   return (
     <View style={styles.weatherView}>
+      <Image style={styles.weatherIcon} source={{ uri: iconUrl }} />
       <Text>{temp}</Text>
     </View>
   );
@@ -16,5 +22,9 @@ export default function WeatherInfo({ weather }) {
 const styles = StyleSheet.create({
   weatherView: {
     alignItems: 'center',
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100,
   },
 });
