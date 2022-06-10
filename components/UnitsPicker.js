1c1c79b23ef8275af5745d44c42774237029afcb
Divine Orji <dpkreativ@gmail.com>
2022-06-10 19:18:46 +0100

    Write styles for `UnitsPicker` - Normal styles - Specific styles for android devices and for ios devices

--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,14 +1,36 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import React from 'react';
 import { Picker } from '@react-native-picker/picker';
 
 export default function UnitsPicker({ tempUnit, setTempUnit }) {
   return (
-    <View>
-      <Picker selectedValue={tempUnit} onValueChange={(e) => setTempUnit(e)}>
+    <View style={styles.unitSystem}>
+      <Picker
+        selectedValue={tempUnit}
+        onValueChange={(e) => setTempUnit(e)}
+        mode="dropdown"
+        itemStyle={{ fontSize: 12 }}
+      >
         <Picker.Item label="C&deg;" value="metric" />
         <Picker.Item label="F&deg;" value="imperial" />
       </Picker>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  unitSystem: {
+    position: 'absolute',
+    ...Platform.select({
+      ios: {
+        top: -20,
+      },
+      android: {
+        top: 20,
+      },
+    }),
+    left: 20,
+    height: 50,
+    width: 100,
+  },
+});
