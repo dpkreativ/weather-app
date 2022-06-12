daeb86c0f80db5a46c31667002732a6ccf67a270
Divine Orji <dpkreativ@gmail.com>
2022-06-13 00:14:56 +0100

    Create component to display weather details

---
+++ components/WeatherDetails.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+export default function WeatherDetails() {
+  return (
+    <View>
+      <Text>WeatherDetails</Text>
+    </View>
+  );
+}
