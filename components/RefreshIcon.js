e76961c7b671fd8404bbd3026324037ce03025e8
Divine Orji <dpkreativ@gmail.com>
2022-06-13 00:10:02 +0100

    Add styles to the refresh icon

--- components/RefreshIcon.js
+++ components/RefreshIcon.js
@@ -1,11 +1,21 @@
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { colors } from '../utils';
 
 export default function RefreshIcon() {
   return (
-    <View>
-      <Ionicons name="refresh" size={24} color="black" />
+    <View style={styles.refreshIcon}>
+      <Ionicons name="refresh" size={24} color={colors.PRIMARY_COLOR} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  refreshIcon: {
+    position: 'absolute',
+    top: 20,
+    right: 20,
+    paddingTop: 20,
+  },
+});
